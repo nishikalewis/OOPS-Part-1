cfcd4922cd2a95584847947e5e27a40a8cad2182
nishikalewis <113398001+nishikalewis@users.noreply.github.com>
2023-03-19 21:42:45 -0400

    Delete hw04 directory

--- hw04/src/edu/umb/cs680/hw04/Authenticator.java
+++
@@ -1,8 +0,0 @@
-package edu.umb.cs680.hw04;
-
-public class Authenticator {
-    public static boolean authenticate(SecurityContext ctx,EncryptedString pwd){
-        return true;
-    }
-
-}
--- hw04/src/edu/umb/cs680/hw04/State.java
+++
@@ -1,6 +0,0 @@
-package edu.umb.cs680.hw04;
-
-public interface State {
-    public  void login(EncryptedString pwd);
-    public void logout();
-}
