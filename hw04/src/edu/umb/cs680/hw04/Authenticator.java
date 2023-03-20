826cd537ee1acf7dc39d5aaab95b4d597dcb2ec5
nishikalewis <113398001+nishikalewis@users.noreply.github.com>
2023-03-19 21:41:09 -0400

    HW04

---
+++ hw04/src/edu/umb/cs680/hw04/Authenticator.java
@@ -0,0 +1,8 @@
+package edu.umb.cs680.hw04;
+
+public class Authenticator {
+    public static boolean authenticate(SecurityContext ctx,EncryptedString pwd){
+        return true;
+    }
+
+}
---
+++ hw04/src/edu/umb/cs680/hw04/EncryptedString.java
@@ -0,0 +1,10 @@
+package edu.umb.cs680.hw04;
+
+public class EncryptedString {
+
+    String pwd;
+    public EncryptedString(String pwd)
+    {
+        this.pwd=pwd;
+    }
+}
