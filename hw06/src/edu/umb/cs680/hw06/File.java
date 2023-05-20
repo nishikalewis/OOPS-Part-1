0a0ba970b281914f7c16e4ec7a02519c3cf35d47
nishikalewis <113398001+nishikalewis@users.noreply.github.com>
2023-05-20 18:34:17 -0400

    HW06

---
+++ hw06/src/edu/umb/cs680/hw06/File.java
@@ -0,0 +1,16 @@
+package edu.umb.cs680.hw06;
+
+import java.time.LocalDateTime;
+
+public class File  extends FSElement{
+    public File(Directory parent, String name, int size, LocalDateTime creationTime) {
+        super(parent, name, size, creationTime);
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+
+
+}
---
+++ hw06/test/src/edu/umb/cs680/hw06/FileTest.java
@@ -0,0 +1,43 @@
+package edu.umb.cs680.hw06;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileTest {
+    private static  FileSystem fs;
+    LocalDateTime time=TestFixtureInitializer.time;
+    @BeforeAll
+    public static void setUP(){
+        fs=TestFixtureInitializer.createFS();
+    }
+
+    private String[] fileoStringArray(File f){
+        String[] Info = {String.valueOf(f.getParent().getName()),
+                f.getName(), String.valueOf(f.getSize()), String.valueOf(f.getCreationTime())};
+        return Info;
+    }
+
+    @Test
+    public void check_file(){
+        Directory root=fs.getRootDir().getFirst();
+        LinkedList<File> files=root.getFiles();
+        String expected[]={"prjRoot","x","5",String.valueOf(time)};
+        String actual[]=fileoStringArray(files.getFirst());
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void check_ifFile(){
+        Directory root=fs.getRootDir().getFirst();
+        LinkedList<Directory> home=root.getSubDirectories();
+        LinkedList<File> files_list=home.get(0).getFiles();
+        assertFalse(files_list.getFirst().isDirectory());
+
+    }
+
+}
